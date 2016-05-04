9d163b7d2327b5bb336ad9fe9796c92429082866
riveracangela@gmail.com <riveracangela@gmail.com>
2016-05-04 17:09:21 -0500

    Fix random bug

--- scripts/beyonce.js
+++ scripts/beyonce.js
@@ -17,7 +17,7 @@ module.exports = function(robot) {
 		response.reply( user.real_name +' doesn\'t want you to win')
 	})
 
-	robot.random(/major key/i, function(response) {
+	robot.hear(/major key/i, function(response) {
 		keyWisdom = ['tell \'em', 'boy bye', '<img src="http://rip3pac.neocities.org/CW2bcETUsAAq_vW.jpg">', 'img src="http://static.wixstatic.com/media/a35832_2f122dc71d454324b9d779a828c507ef.png/v1/fill/w_554,h_473,al_c,lg_1/a35832_2f122dc71d454324b9d779a828c507ef.png">']
 
 		// res.send res.random keyWisdom
